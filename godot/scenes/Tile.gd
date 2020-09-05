f2f1e2be3842186884ac76c2f63600fcfd77208c
erikkarlen <erik.karlen@gmail.com>
2020-09-06 01:15:03 +0200

    Create basic tile scene

---
+++ godot/scenes/Tile.gd
@@ -0,0 +1,23 @@
+extends Node2D
+class_name Tile
+
+enum TileType{
+	NONE,
+	RANDOM,
+	ZERO,
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	ADDITION,
+	SUBTRACTION,
+	MULTIPLYING,
+	DIVISION
+}
+
+var tileType = TileType.NONE
